4283561a1d19e3e83d44f76e1840415498afad1b
Supan Adit Pratama <email@supanadit.com>
2025-06-29 17:25:24 +0700

    feat: implement ModeBloc for timer mode management and integrate with TimerPage

--- lib/features/timer/presentation/page/timer_page.dart
+++ lib/features/timer/presentation/page/timer_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fomoless/features/timer/presentation/bloc/mode_bloc.dart';
 import 'package:fomoless/features/timer/presentation/bloc/timer_bloc.dart';
 import 'package:fomoless/features/timer/presentation/widget/information_widget.dart';
 import 'package:fomoless/features/timer/presentation/widget/time_display_widget.dart';
@@ -9,70 +10,91 @@ class TimerPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => TimerBloc(),
-      child: BlocBuilder<TimerBloc, TimerState>(
-        builder: (context, state) {
-          return Scaffold(
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => TimerBloc()),
+        BlocProvider(create: (_) => ModeBloc()),
+      ],
+      child: BlocBuilder<ModeBloc, ModeState>(
+        builder: (context, modeState) {
+          return BlocBuilder<TimerBloc, TimerState>(
+            builder: (context, timerState) {
+              return Scaffold(
+                body: Center(
+                  child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.grey.withValues(alpha: 180),
-                          shadowColor: Colors.transparent,
-                          surfaceTintColor: Colors.transparent,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(3.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              context.read<ModeBloc>().add(
+                                ModeSwitchRequested(TimerMode.stopwatch),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  modeState.mode == TimerMode.stopwatch
+                                  ? Colors.grey.withAlpha(180)
+                                  : Colors.transparent,
+                              shadowColor: Colors.transparent,
+                              surfaceTintColor: Colors.transparent,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(3.0),
+                              ),
+                              elevation: 0,
+                            ),
+                            child: const Text(
+                              "Stopwatch",
+                              style: TextStyle(color: Colors.black),
+                            ),
                           ),
-                          elevation: 0,
-                        ),
-                        child: const Text(
-                          "Stopwatch",
-                          style: TextStyle(color: Colors.black),
+                          const SizedBox(width: 10),
+                          ElevatedButton(
+                            onPressed: () {
+                              context.read<ModeBloc>().add(
+                                ModeSwitchRequested(TimerMode.pomodoro),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  modeState.mode == TimerMode.pomodoro
+                                  ? Colors.grey.withAlpha(180)
+                                  : Colors.transparent,
+                              shadowColor: Colors.transparent,
+                              surfaceTintColor: Colors.transparent,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(3.0),
+                              ),
+                              elevation: 0,
+                            ),
+                            child: const Text(
+                              "Pomodoro",
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      const TimeDisplayWidget(),
+                      const SizedBox(height: 30),
+                      ElevatedButton(
+                        onPressed: () {
+                          context.read<TimerBloc>().add(TimerStarted());
+                        },
+                        child: Text(
+                          timerState.isRunning ? "Stop Timer" : "Start Timer",
+                          style: const TextStyle(fontSize: 20.0),
                         ),
                       ),
-                      const SizedBox(width: 10),
-                      ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.transparent,
-                          shadowColor: Colors.transparent,
-                          surfaceTintColor: Colors.transparent,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(3.0),
-                          ),
-                          elevation: 0,
-                        ),
-                        child: const Text(
-                          "Pomodoro",
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      ),
+                      const SizedBox(height: 50),
+                      const InformationWidget(),
                     ],
                   ),
-                  SizedBox(height: 20),
-                  const TimeDisplayWidget(),
-                  const SizedBox(height: 30),
-                  ElevatedButton(
-                    onPressed: () {
-                      context.read<TimerBloc>().add(TimerStarted());
-                    },
-                    child: Text(
-                      state.isRunning ? "Stop Timer" : "Start Timer",
-                      style: const TextStyle(fontSize: 20.0),
-                    ),
-                  ),
-                  const SizedBox(height: 50),
-                  const InformationWidget(),
-                ],
-              ),
-            ),
+                ),
+              );
+            },
           );
         },
       ),
--- lib/features/timer/presentation/widget/time_display_widget.dart
+++ lib/features/timer/presentation/widget/time_display_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fomoless/features/timer/presentation/bloc/mode_bloc.dart';
 import 'package:fomoless/features/timer/presentation/bloc/timer_bloc.dart';
 import 'package:fomoless/features/timer/presentation/widget/time_unit_widget.dart';
 
@@ -10,52 +11,60 @@ class TimeDisplayWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<TimerBloc, TimerState>(
-      builder: (context, state) {
-        return InkWell(
-          borderRadius: BorderRadius.circular(10.0),
-          onDoubleTap: () {
-            context.read<TimerBloc>().add(TimerReset());
-          },
-          onTap: () {
-            context.read<TimerBloc>().add(TimerToggleMilliseconds());
-          },
-          child: Container(
-            padding: const EdgeInsets.all(20.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                TimeUnitWidget(value: twoDigits(state.hours)),
-                const Text(":", style: TextStyle(fontSize: 60.0)),
-                TimeUnitWidget(value: twoDigits(state.minutes)),
-                const Text(":", style: TextStyle(fontSize: 60.0)),
-                TimeUnitWidget(value: twoDigits(state.seconds)),
-                Container(
-                  padding: const EdgeInsets.only(bottom: 10.0),
-                  child: Visibility(
-                    visible: !state.hideMilliseconds,
-                    child: Row(
-                      children: [
-                        const Text(
-                          ".",
-                          style: TextStyle(fontSize: 30, color: Colors.grey),
+    return BlocBuilder<ModeBloc, ModeState>(
+      builder: (context, modeState) {
+        return BlocBuilder<TimerBloc, TimerState>(
+          builder: (context, state) {
+            // You can use modeState.mode here to customize display if needed
+            return InkWell(
+              borderRadius: BorderRadius.circular(10.0),
+              onDoubleTap: () {
+                context.read<TimerBloc>().add(TimerReset());
+              },
+              onTap: () {
+                context.read<TimerBloc>().add(TimerToggleMilliseconds());
+              },
+              child: Container(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    TimeUnitWidget(value: twoDigits(state.hours)),
+                    const Text(":", style: TextStyle(fontSize: 60.0)),
+                    TimeUnitWidget(value: twoDigits(state.minutes)),
+                    const Text(":", style: TextStyle(fontSize: 60.0)),
+                    TimeUnitWidget(value: twoDigits(state.seconds)),
+                    Container(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      child: Visibility(
+                        visible: !state.hideMilliseconds,
+                        child: Row(
+                          children: [
+                            const Text(
+                              ".",
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            Text(
+                              state.milliseconds.toString().padLeft(3, '0'),
+                              style: const TextStyle(
+                                fontSize: 30,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
                         ),
-                        Text(
-                          state.milliseconds.toString().padLeft(3, '0'),
-                          style: const TextStyle(
-                            fontSize: 30,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
-          ),
+              ),
+            );
+          },
         );
       },
     );
